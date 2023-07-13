d3e21ff71b78970f76165c7fe6f57527e382f4c2
leonardolima-escolar <l.leonardo@escolar.ifrn.edu.br>
2023-07-13 09:29:54 -0300

    Add list view and import view

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.SolarPanelKits.as_view(), name='solar_panel_kits'),
-    path('/import_to_the_database', views.ImportToTheDatabase.as_view(), name='import_to_the_database'),
+    path('import_to_the_database/', views.ImportToTheDatabase.as_view(), name='import_to_the_database'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 
@@ -10,6 +12,12 @@ class SolarPanelKits(ListView):
     model = Kit
 
 class ImportToTheDatabase(View):
+    def get(self, request):
+        return render(request, 'core/import_to_the_database.html')
+
     def post(self, request):
-        return
+        excel_file = request.FILES['file']
+        df = pd.read_excel(excel_file)
+        print(df['Código'])
+        return render(request, 'core/kit_list.html')
 
